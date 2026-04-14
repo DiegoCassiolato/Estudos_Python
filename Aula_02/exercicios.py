23c70f953662ea18d75ed81672bc8736207b9af7
Diego Hermoso Cassiolato <diego.cassiolato@votorantim.com>
2026-04-14 16:42:54 -0300

    exercicios

--- Aula_02/exercicios.py
+++ Aula_02/exercicios.py
@@ -1,45 +1,128 @@
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
-num_1 =int(input("Insira o numero: "))
-num_2 = int(input("Insira o numero "))
-
-resultado1 = print(num_1 + num_2)
+# num_1 =int(input("Insira o numero: "))
+# num_2 = int(input("Insira o numero "))
+# resultado1 = print(num_1 + num_2)
 
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
-num_1 =int(input("Insira o numero: "))
-num_2 = int(input("Insira o numero: "))
-
-resultado2 = print(num_1 % num_2)
-resultado_final = print(resultado2 / 5)
+# num = int(input("Digite um número: "))
+# num = 18  # Exemplo de entrada
+# resultado_resto = num % 5
+# print("O resto da divisão por 5 é:", resultado_resto)
 
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+# num_1 = float(input("Digite o primeiro número: "))
+# num_2 = float(input("Digite o segundo número: "))
+# resultado = print(num_1 * num_2)
+
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+# num_1 =int(input("Insira o numero: "))
+# num_2 = int(input("Insira o numero: "))
+# resultado = num_1 // num_2
+# print(resultado)
+
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+# num = int(input("Digite um número: "))
+# resultado_quadrado = num ** 2
+# print("O quadrado do número é:", resultado_quadrado)
 
 # #### Números de Ponto Flutuante (`float`)
 
 # 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
+# num_1 = float(input("Digite o primeiro número: "))
+# num_2 = float(input("Digite o segundo número: "))
+# resultado = num_1 + num_2
+# print("O resultado da soma é:", resultado)
+
 # 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+# num_1 = float(input("Digite o primeiro número: "))
+# num_2 = float(input("Digite o segundo número: "))
+# resultado_media = (num_1 + num_2) / 2
+# print("O resultado da média é:", resultado_media)
+
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+# num_base = float(input("Digite o número base: "))
+# expoente = float(input("Digite o número expoente: "))
+# resultado_potencia = num_base ** expoente
+# print("O resultado da potência é:", resultado_potencia) 
+
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+# MULTIPLICADOR_F = 1.8
+# ADICAO_F = 32
+# temp_celsius = int(input("Digite a temperatura em ºC: "))
+# conversao = (temp_celsius * MULTIPLICADOR_F) + ADICAO_F
+# print(f"A temperatura de {temp_celsius}ºC convertida para Fahrenheit é {conversao}ºF")
+
+
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+# raio = float(input("Digite o raio do círculo: "))
+# area = 3.14159 * raio ** 2
+# print("A área do círculo é:", area)
+
 
 # #### Strings (`str`)
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+# texto = input("Digite o texto em minúsculo: ")
+# texto_maiusculo = texto.upper()
+# print("Texto maiúsculo:", texto_maiusculo)
+
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+# nome = input("Digite o nome em maiúsculo: ")
+# texto_minusculo = nome.lower()
+# print("Texto minúsculo:", texto_minusculo)
+
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+# frase = input("Digite a frase: ")
+# frase_strip = frase.strip()
+# print(frase_strip)
+
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+# data_usuario = input("Insira uma data no formato dd/mm/yyy: ")
+# lista_dia_mes_ano = data_usuario.split("/") 
+# print(f"O elemento 1 é: {lista_dia_mes_ano[0]}")
+# print(f"O elemento 2 é: {lista_dia_mes_ano[1]}")      
+# print(f"O elemento 3 é: {lista_dia_mes_ano[2]}")
+
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
+# string_1 = input("Digite a primeira string: ")
+# string_2 = input("Digite a segunda string: ")
+# concat_string = string_1 + string_2
+# print(concat_string)
 
 # #### Booleanos (`bool`)
 
 # 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+# valor1 = True
+# valor2 = False
+# resultado_and = valor1 and valor2
+# print("Resultado do AND lógico:", resultado_and)
+
 # 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+# valor1 = True
+# valor2 = False
+# resultado_or = valor1 or valor2
+# print("Resultado do AND lógico:", resultado_or)
+
 # 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+# valor1 = True
+# valor2 = False
+# resultado_not = not valor1
+# print("Resultado do NOT lógico:", resultado_not)
+
 # 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+# valor1 = int(input("Digite o número: "))
+# valor2 = int(input("Digite o número: "))
+# resultado_igualdade = (valor1 == valor2)
+# print("Resultado igualdade:", resultado_igualdade)
+
 # 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
+# valor1 = int(input("Digite o número: "))
+# valor2 = int(input("Digite o número: "))
+# resultado_diferenca = (valor1 != valor2)
+# print("Resultado diferença:", resultado_diferenca)
+
 
 # #### try-except e if
 
